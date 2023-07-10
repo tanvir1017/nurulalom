7da06fcaca53269c473408218e46b7b57cfd4115
tanvir1017 <tanvir.hossaindev1@gamil.com>
2023-07-10 14:55:23 +0600

    fix: 🐛 Make layout.tsx to RSC
    Make layout.tsx to RSC
    
    BREAKING CHANGE: 🧨 Make layout.tsx to RSC
    
    ✅ Closes: Make layout.tsx to RSC

--- src/components/pages/homepage/home.tsx
+++ src/components/pages/homepage/home.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { ThemeProvider } from "@material-tailwind/react";
 import Consulting from "./consulting/consulting";
 import GetMoreLeads from "./getmoreleads/getmoreleads";
 import Introduce from "./introduce/introduce";
@@ -6,14 +8,14 @@ import TrustedBy from "./trustedby/trustedby";
 const HomePage = () => {
   return (
     <main className="App">
-      <>
+      <ThemeProvider>
         <Introduce />
         <TrustedBy />
         <GetMoreLeads />
         <div className="my-32 bg-[#F3F7FB]">
           <Consulting />
         </div>
-      </>
+      </ThemeProvider>
     </main>
   );
 };
